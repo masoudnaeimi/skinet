fdd8a4349d4d6cae8515387a3efd0b940c2776de
Masoud <masoudweb@gmail.com>
2020-04-29 08:55:08 +0430

    End Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,4 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -11,6 +12,8 @@ namespace API
     public class Startup
     {
         private readonly IConfiguration _config;
+        private int iProductRepository;
+
         public Startup(IConfiguration config)
         {
             _config = config;
@@ -19,9 +22,10 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IProductRepository, ProductRepository>();
             services.AddControllers();
             services.AddDbContext<StoreContext>(x =>
-                     x.UseSqlServer(_config.GetConnectionString("DefaultConnection")));
+                     x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Core.Entities;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+namespace Infrastructure.Data
+{
+    public class StoreContextSeed
+    {
+        public static async Task SeedAsync(StoreContext context, ILoggerFactory loggerFactory)
+        {
+            try
+            {
+                if (!context.ProductBrands.Any())
+                {
+                    string brandsData = File.ReadAllText("../Infrastructure/Data/SeedData/brands.json");
+                    var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
+                    
+                    foreach (var item in brands)
+                    {
+                        context.ProductBrands.Add(item);
+                    }
+                    await context.SaveChangesAsync();
+                }
+
+                if (!context.ProductTypes.Any())
+                {
+                    string typeData = File.ReadAllText("../Infrastructure/Data/SeedData/types.json");
+                    var types = JsonSerializer.Deserialize<List<ProductType>>(typeData);
+                    foreach (var item in types)
+                    {
+                        context.ProductTypes.Add(item);
+                    }
+                    await context.SaveChangesAsync();
+                }
+
+                if (!context.Products.Any())
+                {
+                    string productData = File.ReadAllText("../Infrastructure/Data/SeedData/products.json");
+                    var products = JsonSerializer.Deserialize<List<Product>>(productData);
+                    foreach (var item in products)
+                    {
+                        context.Products.Add(item);
+                    }
+                    await context.SaveChangesAsync();
+                }
+            }
+            catch (Exception err)
+            {
+                var logger = loggerFactory.CreateLogger<StoreContextSeed>();
+                logger.LogError(err.Message);
+            }
+        }
+    }
+}
