fffc4e8a0221c262165d37a5fc1e889d80804283
Masoud <daya@MASOUDWEB>
2020-05-10 17:09:01 +0430

    End of section 8

--- API/Startup.cs
+++ API/Startup.cs
@@ -37,6 +37,14 @@ namespace API
 
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,6 +61,8 @@ namespace API
             app.UseStaticFiles();
 
             app.UseAuthorization();
+
+            app.UseCors("CorsPolicy");
 
             app.UseSwaggerDocumentation();
 
